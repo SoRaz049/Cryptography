e2d9c1818e53e391cdeebb1270d718c2e926623b
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2023-08-30 16:01:48 +0545

    encryption complete

---
+++ encrypt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+string upperlower(int ch, char check)
+{
+    string cypher;
+
+    if((ch) > check)
+        cypher+= ch- 26 ;
+    else
+        cypher+=ch;
+
+    return cypher;
+}
+
+string encrpyt(string message, int key)
+{
+    int i,message_int;
+    string cypher;
+
+    for(i=0;i<message.length();i++)
+    {
+        message_int = message[i];
+
+        if(message_int>=97 && message_int<=122)
+            cypher+=upperlower(message_int+key,'z');
+
+        else if(message_int>=65 && message_int<=90)
+            cypher+=upperlower(message_int+key,'Z');
+    }
+
+    cout<<cypher;
+    return cypher;
+}
+
+int main()
+{
+    encrpyt("Nayan",14);
+}
